b49e57f353a13d7c08f097ffd050d98965045a6c
Codexzier <johannes.paul.langner@live.de>
2025-01-30 09:26:15 +0100

    add chatbot dialog options.

--- ExampleLlama/ChatbotLlama.cs
+++ ExampleLlama/ChatbotLlama.cs
@@ -7,7 +7,8 @@ namespace ExampleLlama;
 
 public class ChatbotLlama : IDisposable
 {
-    private readonly string _fileHistorie = $"{Environment.CurrentDirectory}/chatHistory.txt";
+    private readonly string _assistanceName;
+    private readonly string _fileHistorie = $"{Environment.CurrentDirectory}/chatHistory_";
     
     private readonly InteractiveExecutor _executor;
     private ChatHistory _chatHistory;
@@ -16,19 +17,20 @@ public class ChatbotLlama : IDisposable
     private readonly LLamaWeights _model;
     private readonly LLamaContext _context;
 
-    public ChatbotLlama(string modelPath)
+    public ChatbotLlama(string modelPath, string assistanceName)
     {
+        this._assistanceName = assistanceName;
         var parameters = new ModelParams(modelPath)
         {
             ContextSize = 1024, // The longest length of chat as memory.
             GpuLayerCount = 5, // How many layers to offload to GPU. Please adjust it according to your GPU memory.
         };
-        
-        _model = LLamaWeights.LoadFromFile(parameters);
-        _context = _model.CreateContext(parameters);
-        _executor = new InteractiveExecutor(_context);
-        
-        _inferenceParams = new InferenceParams
+
+        this._model = LLamaWeights.LoadFromFile(parameters);
+        this._context = _model.CreateContext(parameters);
+        this._executor = new InteractiveExecutor(_context);
+
+        this._inferenceParams = new InferenceParams
         {
             SamplingPipeline = new DefaultSamplingPipeline(), // use default sampling pipeline
             MaxTokens = 256, // maximum number of tokens to generate
@@ -36,22 +38,30 @@ public class ChatbotLlama : IDisposable
         };
     }
     
+    private string GetHistoryFileName()
+    {
+        return $"{this._fileHistorie}{this._assistanceName}.json";
+    }
+    
     public List<ChatItem> LoadChatHistory(bool loadFromFile = true, string systemRoleDescription = "")
     {
-        if (loadFromFile && File.Exists(_fileHistorie))
+        if (loadFromFile && File.Exists(this.GetHistoryFileName()))
         {
-            var chatHistory = File.ReadAllText(_fileHistorie);
-            _chatHistory = ChatHistory.FromJson(chatHistory) ?? new ChatHistory();
+            var chatHistory = File.ReadAllText(this.GetHistoryFileName());
+            this._chatHistory = ChatHistory.FromJson(chatHistory) ?? new ChatHistory();
         }
         else
         {
-            _chatHistory = new ChatHistory();
-            _chatHistory.AddMessage(AuthorRole.System, systemRoleDescription);
+            this._chatHistory = new ChatHistory();
+            this._chatHistory.AddMessage(AuthorRole.System, systemRoleDescription);
         }
-            
-        _session = new ChatSession(_executor, _chatHistory); 
+
+        this._session = new ChatSession(this._executor, this._chatHistory); 
         
-        List<ChatItem> chatItems = _chatHistory.Messages.Select(s => new ChatItem(s.AuthorRole.ToString(), s.Content)).ToList();
+        var chatItems = this._chatHistory
+            .Messages
+            .Select(s => new ChatItem(s.AuthorRole.ToString(), s.Content))
+            .ToList();
         return chatItems;
     }
     
@@ -68,13 +78,13 @@ public class ChatbotLlama : IDisposable
 
     public void Dispose()
     {
-        _model.Dispose();
-        _context.Dispose();
+        this._model.Dispose();
+        this._context.Dispose();
     }
 
     public void SaveChatHistory()
     {
-        File.WriteAllText(_fileHistorie, _chatHistory.ToJson());
+        File.WriteAllText(this.GetHistoryFileName(), this._chatHistory.ToJson());
     }
 }
 
--- ExampleLlama/Program.cs
+++ ExampleLlama/Program.cs
@@ -7,14 +7,114 @@ namespace ExampleLlama
         private static ChatbotLlama _chatbotLlamaType2;
         private static ChatbotLlama _chatbotLlamaType1;
 
+        private const string ModelPath1 = "C:/Users/Codexzier/.lmstudio/models/lmstudio-community/Phi-3.1-mini-128k-instruct-GGUF/Phi-3.1-mini-128k-instruct-Q4_K_M.gguf";
+        private const string ModelPath= "C:/Users/JohannesPaulLangner/.lmstudio/models/lmstudio-community/Phi-3.1-mini-128k-instruct-GGUF/Phi-3.1-mini-128k-instruct-Q4_K_M.gguf";
+
         static async Task Main(string[] args)
         {
-            string modelPath = "C:/Users/Codexzier/.lmstudio/models/lmstudio-community/Phi-3.1-mini-128k-instruct-GGUF/Phi-3.1-mini-128k-instruct-Q4_K_M.gguf";
+           Console.WriteLine("Welcome to the ChatbotLlama example!");
+           Console.WriteLine("Choose chatbot type: ");
+              Console.WriteLine("1. Aouda");
+                Console.WriteLine("2. Titus");
+                Console.WriteLine("3. Two chatbots talking to each other");
+                Console.WriteLine("4 .Exit");
+              Console.WriteLine("Type 'exit' to close the chat session.");
+            Console.WriteLine();
+            
+            var input = Console.ReadLine();
 
-            StartChatBotTyp1(modelPath);
+            switch (input)
+            {
+                case "1":
+                    await ChatWithBot();
+                    break;
+                case "3":
+                    await ChatTalkToEachOther();
+                    break;
+                case "4":
+                    break;
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async Task ChatTalkToEachOther()
+        {
+            _chatbotLlamaType1 = new ChatbotLlama(ModelPath, "Remus1");
+            var systemRoleDescription = new StringBuilder(); 
+            systemRoleDescription.AppendLine("Transcript of a dialog, where the User interacts with an Assistant named Remus. ");
+            systemRoleDescription.AppendLine("Remus ist ein Geschichtenerzähler und verwendet Historische Elemente aus dem Mittelalter und der zweiten Industriellen Revolution.");
+            systemRoleDescription.AppendLine("Die Antworten, die ein Teil der Geschichte sind, werden in ein bis zwei Sätzen wieder gegeben.");
+            // systemRoleDescription.AppendLine("Remus ist technisch versiert und spricht sich offen über seine Gedanken aus.");
+            // systemRoleDescription.AppendLine("Remus stellt auch Fragen, um mehr über die Anforderung zu erfahren.");
+            // systemRoleDescription.AppendLine("Remus ist ein Entwickler und hat Erfahrung in der Entwicklung von Komponenten.");
+            // systemRoleDescription.AppendLine("Remus antwortet nicht mit Quellcode, sondern erklärt die Lösung in einfachen Worten."); 
             
+            var history1 = _chatbotLlamaType1.LoadChatHistory(true, systemRoleDescription.ToString());
+            string userInput1 = string.Empty;
+            _chatbotLlamaType1.ChatAnswerEvent += (text) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(text);
+                userInput1 = text;
+            };
+
             
-           
+            _chatbotLlamaType2 = new ChatbotLlama(ModelPath, "Romulus2");
+            var systemRoleDescription2 = new StringBuilder();
+            systemRoleDescription2.AppendLine("Transcript of a dialog, where the User interacts with an Assistant named Romulus. ");
+            systemRoleDescription2.AppendLine("Romulus fragt nach Geschichten und was als nächstes kommt.");
+            systemRoleDescription2.AppendLine("Romulus nach 5 bis 7 erhaltenden Antworten, gibt ein Ziel zu der Geschichte hinzu.");
+            // systemRoleDescription2.AppendLine("Romulus ist Anforderungsmanager.");
+            // systemRoleDescription2.AppendLine("Romulus stellt das Thema Komponenten Entwicklung für EUDR vor.");
+            // systemRoleDescription2.AppendLine("Das Thema EUDR muss in das bestehende System integriert werden.");
+            // systemRoleDescription2.AppendLine("Das Thema EUDR soll als Komponente funktionieren, so das später eine Migration nur wenig Aufwand besteht.");
+            // systemRoleDescription2.AppendLine("Ziel des Thema ist, eine Lösung zu finden, die sich in 10 Sätzen erklären lässt.");
+            
+            var history = _chatbotLlamaType2.LoadChatHistory(true, systemRoleDescription.ToString());
+            foreach (var chatItem in history)
+            {
+                Console.ForegroundColor = chatItem.Role switch
+                {
+                    "User" => ConsoleColor.Green,
+                    "System" => ConsoleColor.Blue,
+                    _ => ConsoleColor.White
+                };
+                Console.WriteLine($"{chatItem.Role}: {chatItem.Message}");
+            }
+            
+            string userInput2 = string.Empty;
+            _chatbotLlamaType2.ChatAnswerEvent += (text) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(text);
+                userInput2 = text;
+            };
+            
+            userInput1 = "Heute möchte ich eine Geschichte über eine Fantastisches Abenteuer im 19. Jahrhundert.";
+
+            while (userInput2 != "exit")
+            {
+                await _chatbotLlamaType2.Chat(userInput1);
+                Console.WriteLine();
+                
+                await Task.Delay(3000);
+                
+                await _chatbotLlamaType1.Chat(userInput2);
+                Console.WriteLine();
+                
+                await Task.Delay(3000);
+
+            }
+            
+            _chatbotLlamaType1.SaveChatHistory();
+            _chatbotLlamaType2.SaveChatHistory();
+        }
+
+        private static async Task ChatWithBot()
+        {
+            StartChatBotTyp1(ModelPath);
             //StartChatBotTyp2(modelPath);
             
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -35,14 +135,11 @@ namespace ExampleLlama
             }
             
             _chatbotLlamaType1.SaveChatHistory();
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
         }
 
         private static void StartChatBotTyp1(string modelPath)
         {
-            _chatbotLlamaType1 = new ChatbotLlama(modelPath);
+            _chatbotLlamaType1 = new ChatbotLlama(modelPath, "Aouda");
             
             var systemRoleDescription = new StringBuilder();
             // Aouda
@@ -51,8 +148,8 @@ namespace ExampleLlama
             systemRoleDescription.AppendLine("Aouda ist ehrlich, ist jedoch etwas ausfallend in ihrere Beschreibt die etwas fantastisch wirken!");
             systemRoleDescription.AppendLine("Aouda ist auch eine Abenteuerin und fügt bei ihren Antworten noch zwei bis drei Sätze aus einem bekannten Abenteuer hinzu");
             
-            var historie = _chatbotLlamaType1.LoadChatHistory(true, systemRoleDescription.ToString());
-            foreach (var chatItem in historie)
+            var history = _chatbotLlamaType1.LoadChatHistory(true, systemRoleDescription.ToString());
+            foreach (var chatItem in history)
             {
                 Console.ForegroundColor = chatItem.Role switch
                 {
